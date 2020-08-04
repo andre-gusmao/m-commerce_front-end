50bef64256595ce6e7106019e53edc71e4de25b7
Timoteo <timoteobega@gmail.com>
2020-08-03 22:59:46 -0300

    Criado a mensagem de agradecimento para o feedback

--- src/app/customers/customers-feedback/customers-feedback.page.ts
+++ src/app/customers/customers-feedback/customers-feedback.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { AuthenticationsService } from '../../services/authentications/authentications.service';
 import { ToolsService } from 'src/app/services/tools/tools.service';
-import { RequestsService } from '../../services/requests/requests.service';
 
 @Component({
   selector: 'app-customers-feedback',
@@ -12,22 +10,19 @@ import { RequestsService } from '../../services/requests/requests.service';
 export class CustomersFeedbackPage implements OnInit {
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    public requestService: RequestsService,
     public toolsService: ToolsService,
     public authService: AuthenticationsService
   ) { }
 
-  ngOnInit() {}
-
-  ionViewWillEnter() {
-    if (this.authService.getLoginSuccessful()) {
-      //
-    } else {
+  ngOnInit() {
+    if (!this.authService.getLoginSuccessful()) {
       this.authService.setLogout();
     }
   }
 
-  public registerFeedback(){}
+  public registerFeedback(){
+    this.toolsService.showToast("Obrigado pelo feedback!");
+    this.toolsService.goToPage("/customers-catalogs/");
+  }
 
 }
