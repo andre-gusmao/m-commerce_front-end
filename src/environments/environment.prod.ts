e55591d011a05825697d21b05ad91f7e44173f2b
Timoteo <timoteobega@gmail.com>
2020-08-18 21:30:03 -0300

    Definiçao de tres ambientes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  // endpointURL: "http://kioskwide.com.br/api/"
-  endpointURL: "http://54.232.47.219/api/"
+  endpointURL: "http://kioskwide.com.br/api/"
+  // endpointURL: "http://54.232.47.219/api/"
+  // endpointURL: "http://192.168.0.20/api/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  endpointURL: "http://192.168.0.20/api/"
+  endpointURL: "http://kioskwide.com.br/api/"
+  // endpointURL: "http://54.232.47.219/api/"
+  // endpointURL: "http://192.168.0.20/api/"
 };
 
 /*
