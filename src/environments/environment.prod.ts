8d76f7fc65b71de60c3f8641a50839a1e18e7064
Timoteo <timoteobega@gmail.com>
2020-08-20 21:18:34 -0300

    Uso do https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  endpointURL: "http://kioskwide.com.br/api/"
-  // endpointURL: "http://54.232.47.219/api/"
-  // endpointURL: "http://192.168.0.20/api/"
+  endpointURL: "https://kioskwide.com.br/api/"//000webhost
+  // endpointURL: "http://54.232.47.219/api/"//aws
+  // endpointURL: "http://192.168.0.20/api/"//local
 };
