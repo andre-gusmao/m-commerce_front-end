ed73826bb090914f881a5bb31ab47983ff7b3bcd
Timoteo <timoteobega@gmail.com>
2020-08-23 21:46:55 -0300

    Removido o model de grupos de produtos

--- src/app/companies/products-categories-list/products-categories.model.ts
+++
@@ -1,8 +0,0 @@
-export interface ProductsCategories {
-    success: boolean;
-    result: [{
-        id_product_category?: number;
-        id_company?: number;
-        product_category_name?: string;
-    }]
-}
