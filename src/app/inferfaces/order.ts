e341394f8eabd5bbc0cdb2d454eac25bb1b910b6
Timoteo <timoteobega@gmail.com>
2020-08-29 10:28:20 -0300

    Atributos das interfaces como opcionais

--- src/app/inferfaces/order.ts
+++ src/app/inferfaces/order.ts
@@ -1,11 +1,11 @@
 export interface IOrder {
-    order_name: string;
-    id_company: number;
-    id_customer: number;
-    order_item_quantity: number;
-    order_total_price: number;
-    order_status: number;
-    order_payment_status: number;
-    order_payment_method: string;
-    id_payment_method: number;
+    order_name?: string;
+    id_company?: number;
+    id_customer?: number;
+    order_item_quantity?: number;
+    order_total_price?: number;
+    order_status?: number;
+    order_payment_status?: number;
+    order_payment_method?: string;
+    id_payment_method?: number;
 }
--- src/app/inferfaces/orderItem.ts
+++ src/app/inferfaces/orderItem.ts
@@ -1,10 +1,10 @@
 export interface IOrderItem {
-    order_item_name: string;
-    id_catalog: number;
-    id_product: number;
-    item_product_name: string;
-    item_quantity: number;
-    item_unit_price: number;
-    item_total_price: number;
-    item_note: string;
+    order_item_name?: string;
+    id_catalog?: number;
+    id_product?: number;
+    item_product_name?: string;
+    item_quantity?: number;
+    item_unit_price?: number;
+    item_total_price?: number;
+    item_note?: string;
 }
