85d9e296212a35f5d940ca7f28c8f202df646b0d
Timoteo <timoteobega@gmail.com>
2020-11-06 21:41:31 -0300

    Inclusão do ambiente da locaweb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,6 @@ export const environment = {
   passwordYaPay: "k%3Fab3A",
   // endpointURL: "https://kioskwide.com.br/api/"//000webhost
   // endpointURL: "http://54.232.47.219/api/"//aws
-  endpointURL: "http://192.168.0.20/api/"//local
+  // endpointURL: "http://192.168.0.20/api/"//local
+  endpointURL: "http://appkioskwide1.hospedagemdesites.ws/api/"//locaweb
 };
