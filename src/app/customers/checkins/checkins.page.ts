8f93c032436d91a2b96135df338371a2c26225a6
Timoteo <timoteobega@gmail.com>
2020-12-02 23:15:05 -0300

    Implementado a funçao para retornar o nome do quiosque

--- src/app/customers/checkins/checkins.page.ts
+++ src/app/customers/checkins/checkins.page.ts
@@ -5,6 +5,7 @@ import { ToolsService } from 'src/app/services/tools/tools.service';
 import { AuthenticationsService } from 'src/app/services/authentications/authentications.service';
 import { ShoppingCartService } from 'src/app/services/shopping-cart/shopping-cart.service';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-checkins',
@@ -16,6 +17,8 @@ export class CheckinsPage implements OnInit {
   QRCodeText: string = "";
   tableID: string = "";
   id_company: string = "";
+  userName: string = "";
+  companyName: string = "";
 
   constructor(
     public requestService: RequestsService,
@@ -31,6 +34,8 @@ export class CheckinsPage implements OnInit {
   ionViewWillEnter() {
     if (this.authService.getLoginSuccessful()) {      
       this.tableID = this.authService.getTableID();
+      this.userName = this.authService.getUserName();
+      this.companyName = this.authService.getCompanyName();
     } else {
       this.authService.setLogout();
     }
@@ -46,6 +51,7 @@ export class CheckinsPage implements OnInit {
         this.toolsService.showToast("Código inválido");
       }
     }).catch(error => {
+      this.toolsService.showToast("Não foi possível fazer checkin",2000,"warning");
       console.log("error",error);
     })
   }
@@ -57,7 +63,7 @@ export class CheckinsPage implements OnInit {
       inputs: [{
         name: 'QRCodeText',
         type: 'text',
-        value: 'Q1M10',
+        value: 'Q5M12',
         placeholder: 'Digite aqui o codigo'
       }],
       buttons: [{
@@ -85,6 +91,7 @@ export class CheckinsPage implements OnInit {
 
   public validCatalog(QRCodeText: string): boolean {
     let valid: boolean = true;
+    let url: string = "companies/companies.php?id=" + this.id_company + "?checkin=1";
     this.authService.setCompanyID("");
     if (QRCodeText.length > 0) {
       if ((QRCodeText.substr(0, 1).toLocaleUpperCase() != "Q") || (QRCodeText.indexOf('M') == 0) ) {
@@ -94,8 +101,24 @@ export class CheckinsPage implements OnInit {
         valid = false;
       }
       this.id_company = QRCodeText.substr(1,QRCodeText.indexOf('M')-1);
+      this.requestCompanyName(this.id_company);
       this.authService.setCompanyID(this.id_company);
       return valid;
+  }
+
+  private async requestCompanyName(id_company:string = ""){
+    let url: string = environment.endpointURL + "companies/companies.php?id=" + id_company + "?checkin=1";
+    return await new Promise(res => {
+      let companyName: string = "";
+      this.requestService.getRequest(url).subscribe( dataRes => {
+        if (dataRes['success']) {
+          this.authService.setCompanyName(dataRes['companyName']);
+        }
+      }, error => {
+        this.toolsService.showToast("Não foi possível concluir o checkin",2000,"warning");
+        console.error(error);
+      })
+    });
   }
 
   public checkOut() {
