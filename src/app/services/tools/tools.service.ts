76b5878545435549f71db98c338783ec7115a284
Timoteo <timoteobega@gmail.com>
2020-12-20 12:01:34 -0300

    Implementaçao da funçao isValidCNPJ

--- src/app/services/tools/tools.service.ts
+++ src/app/services/tools/tools.service.ts
@@ -234,7 +234,79 @@ export class ToolsService {
   }
 
   public isValidCNPJ(cnpj){
-    return true;
+    let isValid: boolean = true;
+    let tamanho: number = 0;
+    let numeros: string = '';
+    let digitos: string = '';
+    let soma: number = 0;
+    let pos: number = 0;
+    let i: number = 0;
+    let resultado: number = 0;
+    cnpj = cnpj.replace(/[^\d]+/g,'');
+
+    if(cnpj == '') {
+      isValid = false;
+    }
+    
+    if (cnpj.length != 14){
+      isValid = false;
+    }
+    
+    if (cnpj == "00000000000000" || 
+        cnpj == "11111111111111" || 
+        cnpj == "22222222222222" || 
+        cnpj == "33333333333333" || 
+        cnpj == "44444444444444" || 
+        cnpj == "55555555555555" || 
+        cnpj == "66666666666666" || 
+        cnpj == "77777777777777" || 
+        cnpj == "88888888888888" || 
+        cnpj == "99999999999999") {
+        isValid = false;
+    }
+
+    tamanho = cnpj.length - 2;
+    numeros = cnpj.substring(0,tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+
+    for (i = tamanho; i >= 1; i--) {
+      soma += Number(numeros.charAt(tamanho - i)) * pos--;
+      if (pos < 2) {
+        pos = 9;
+      }
+    }
+
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+
+    if (resultado != Number(digitos.charAt(0)) ){
+      isValid = false;
+    }
+    
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0,tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+
+    for (i = tamanho; i >= 1; i--) {
+      soma += Number(numeros.charAt(tamanho - i)) * pos--;
+      if (pos < 2){
+        pos = 9;
+      }
+    }
+
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+
+    if (resultado != Number(digitos.charAt(1)) ){
+      isValid = false;
+    }
+
+    if(!isValid){
+      this.showToast("CNPJ informado é inválido");
+    }
+
+    return isValid;
   }
 
   public numericOnly(event): boolean {
